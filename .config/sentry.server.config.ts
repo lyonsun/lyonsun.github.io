d5798a976dbbb487fbd68154e4a0985b6b5ec14b
Liang Sun <sunly917@gmail.com>
2025-08-02 12:05:42 +0300

    build: config sentry

---
+++ .config/sentry.server.config.ts
@@ -0,0 +1,9 @@
+import * as Sentry from '@sentry/astro';
+
+Sentry.init({
+    dsn: 'https://b58f88718824de018a5bc4aa1d0dad0f@o4509667338485760.ingest.de.sentry.io/4509667353034832',
+
+    // Adds request headers and IP for users, for more info visit: for more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
+});
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+import sentry from '@sentry/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+    integrations: [
+        react(),
+        sentry({
+            clientInitPath: '.config/sentry.client.config.ts',
+            serverInitPath: '.config/sentry.server.config.ts',
+            sourceMapsUploadOptions: {
+                project: 'javascript-astro',
+                org: 'suns-project',
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+            },
+        }),
+    ],
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        // @ts-expect-error
+        plugins: [tailwindcss()],
+    },
 });
+
